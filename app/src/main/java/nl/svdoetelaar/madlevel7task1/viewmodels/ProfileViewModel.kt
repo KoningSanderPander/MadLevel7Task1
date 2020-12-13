c57356e17d914f4053d9796efe01df558df41e37
Sander <sandervandenoetelaar@hotmail.com>
2020-12-13 23:34:06 +0100

    viewmodel

---
+++ app/src/main/java/nl/svdoetelaar/madlevel7task1/viewmodels/ProfileViewModel.kt
@@ -0,0 +1,48 @@
+package nl.svdoetelaar.madlevel7task1.viewmodels
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import nl.svdoetelaar.madlevel7task1.models.Profile
+import nl.svdoetelaar.madlevel7task1.repositories.ProfileRepository
+
+class ProfileViewModel(application: Application) : AndroidViewModel(application) {
+    private val TAG = "FIRESTORE"
+    private val profileRepository = ProfileRepository()
+
+    val profile = profileRepository.profile
+    val createSucces = profileRepository.createSuccess
+
+    private val _errorText: MutableLiveData<String> = MutableLiveData()
+    val errorText: LiveData<String> get() = _errorText
+
+    fun getProfile() {
+        viewModelScope.launch {
+            try {
+                profileRepository.getProfile()
+            } catch (e: ProfileRepository.ProfileRetrievalError) {
+                val errorMsg = "Something went wrong while retrieving profile"
+                Log.e(TAG, e.message ?: errorMsg)
+                _errorText.value = errorMsg
+            }
+        }
+    }
+
+    fun createProfile(profile: Profile) {
+        viewModelScope.launch {
+            try {
+                profileRepository.createProfile(profile)
+            } catch (e: ProfileRepository.ProfileSaveError) {
+                val errorMsg = "Something went wrong while saving profile"
+                Log.e(TAG, e.message ?: errorMsg)
+                _errorText.value = errorMsg
+            }
+        }
+    }
+
+
+}
